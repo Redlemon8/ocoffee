8bcbf1ebc00d591b5bac28d9eebf8d59b33d441e
redlemon8 <hanquezbenjamin@gmx.com>
2025-02-11 10:12:01 +0100

    update: init express ejs and router and rend the index view,  refactor: move images from demande-client/assets to public/images folder and ajust the css path

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 import 'dotenv/config';
+
+import express from 'express';
+import router from './app/router.js';
+import path from "node:path";
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(import.meta.dirname, "./app/views"));
+
+app.use(express.static(path.join(import.meta.dirname, "public")));
+
+app.use(router);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
