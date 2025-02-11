71ffd76816a74cf6e0e8bb472d088c30eb19c45a
redlemon8 <hanquezbenjamin@gmx.com>
2025-02-11 13:34:02 +0100

    add: database created

--- app/router.js
+++ app/router.js
@@ -6,4 +6,12 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/catalogue", (req, res) => {
+  res.render("catalogue");
+});
+
+router.get("/produit", (req, res) => {
+  res.render("produit");
+});
+
 export default router;
