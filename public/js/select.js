177e2fcc0d3af7c3ad95d0f0838df29d05ac3152
redlemon8 <hanquezbenjamin@gmx.com>
2025-02-17 01:08:23 +0100

    style: reduce on hight size screen

---
+++ public/js/select.js
@@ -0,0 +1,41 @@
+initSeeMoreProductsButton();
+initCategoryFilter();
+
+function initSeeMoreProductsButton() {
+  const selectAllProducts = document.getElementById("select-all-products");
+  selectAllProducts.addEventListener("click", showAllSelectedProducts);  
+}
+
+function initCategoryFilter() {
+  const category = document.getElementById("category");
+  category.addEventListener("change", (event) => {
+    const categoryId = event.target.value;
+
+    const allProducts = document.querySelectorAll("");
+    allProducts.forEach(section => section.classList.remove("hidden"));
+
+    if (categoryId === "*") { 
+      return; 
+    }
+  
+    allProducts.forEach(section => {
+      if (section.dataset.categoryId !== categoryId) {
+        section.classList.add("hidden");
+      }
+    });
+
+    showAllSelectedProducts();
+  });
+}
+
+
+function showAllSelectedProducts() {
+  const discoverContainer = document.getElementById("discover-container");
+  discoverContainer.classList.remove("items-container");
+
+  const selectAllProducts = document.getElementById("select-all-products");
+  if (! selectAllProducts) { 
+    return; 
+  }
+  selectAllProducts.remove();
+}
